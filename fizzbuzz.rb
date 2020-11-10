ee45c3ace325b980f33bb843029fb50e4d6028d8
Austin Hernandez <ahernandez1170@gmail.com>
2020-11-10 22:07:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
 def fizzbuzz(int)
   if int % 3 == 0 && int 5 == 0
   end
 end
 
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz()
+if int % 3 == 0
+  return "fizz"
+end
+
+if int % 5 == 0
+  return "buzz"
+end
